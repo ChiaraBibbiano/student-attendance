a65d8eeeb269246b5fbd7849313b3314170afd13
ChiaraBibbiano <chiara.bibbiano@student.hepl.be>
2026-05-12 17:07:25 +0200

    Add single student show functionality

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -28,5 +28,46 @@ class StudentController
     {
         //stocker un étudiant dans la db
         //demander au navigateur de se rediger vers la page de résultat souhaitée
+        $this->check_csrf();
+        // Valider les données associées à la requête
+
+        // Stocker un étudiant en DB
+        $student = new Student();
+
+        $student->first_name = $_POST['first_name'];
+        $student->last_name = $_POST['last_name'];
+        $student->email = $_POST['email'];
+        $student->matricule = $_POST['matricule'];
+        $student->birth_date = empty($_POST['birth_date']) ? null : $_POST['birth_date'];
+
+        $student->save();
+
+        // Demander au navigateur de se rediriger vers la page de résultat souhaitée
+        Response::redirect('Location: /etudiant?id=' . $student->id);
     }
+    public function show()
+    {
+        // Validation
+        if(!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+            die('Bad Request');
+        }
+        // Sanitisation / Nettoyage | Préparation
+        $id = (int)$_GET['id'];
+
+
+        //récup les données
+        $student = Student:: find($id);
+
+        $title = 'La fiche de ' . $student->first_name;
+
+        view('students.show',
+            compact(
+                'title',
+                'student'
+            )
+        );
+    }
+
+
+
 }
